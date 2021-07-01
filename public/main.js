35d28ec00d6176ef7eb12c4d021c9388c616cfed
LTGPanda <jessehoebe@live.nl>
2021-07-01 16:43:37 +0200

    mans got shapes

--- public/main.js
+++ public/main.js
@@ -2,5 +2,16 @@
 
 window.addEventListener("load", ()=>
 {
-    let HexMal = document.getElementById("hexagon");
+    let HexMal = document.createElement("div");
+    let HexaBox = document.querySelector(".hexcon");
+
+    HexMal.classList.add("hexagon");
+    console.log(HexaBox);
+    console.log(HexMal);
+
+    HexaBox.appendChild(HexMal.cloneNode(true));
+    HexaBox.appendChild(HexMal.cloneNode(true));
+    HexaBox.appendChild(HexMal.cloneNode(true));
+    HexaBox.appendChild(HexMal.cloneNode(true));
+    HexaBox.appendChild(HexMal.cloneNode(true));
 });
