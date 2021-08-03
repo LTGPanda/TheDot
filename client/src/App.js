1f4a0bb71d07d9298875a627b9ff29da53bce0ef
LTGPanda <jessehoebe@live.nl>
2021-08-03 17:01:09 +0200

    hexa setupp json fuckup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-//import Hexagon from 'react-hexagon';
+import Hexagon from 'react-hexagon';
 
 class App extends Component {
   constructor(props) {
@@ -12,36 +12,44 @@ class App extends Component {
     fetch("http://localhost:9000/ServerIndex")
         .then(res => res.json())
         .then(res => this.setState({ apiResponse: res}));
-      //console.log(fetch("http://localhost:9000/ServerIndex"));
   }
 
   componentWillMount(){
     this.callAPI();
   }
 
+  RowBuilder(){
+    return <h1>owo</h1>;
+    //return <Grid container direction="row" className="row"></Grid>
+  }
+
+  MaxCollCounter(TheGrid) {
+    let lenny = 1;
+    console.log(TheGrid);//måste converta json till iterateble array
+
+    //var TheList = this.state.apiResponse.GridList;
+
+    /*for (let index = 0; index < TheGrid.length; index++) {
+      /*const item = TheGrid[index];
+      if (lenny > item.Grid[1]){
+        lenny = item.Grid[1];
+      }
+      console.log("wow");
+    }*/
+    return lenny;
+  }
+
   render() {
-    console.log(this.state.apiResponse.test);
-    if (this.state.apiResponse.test) {
-      return (
-        <div className="App">
-            <h1>woowzers tu</h1>
-        </div>
-      );  
+    //console.log(this.state.apiResponse.GridList);
+    var Colls = [];
+    let lenny = this.MaxCollCounter(this.state.apiResponse.GridList);
+
+    for (let index = 0; index < lenny; index++) {
+      Colls.push(this.RowBuilder());
     }
-    else if (!this.state.apiResponse.test){
-      return (
-        <div className="App">
-            <h1>nah famsquad</h1>
-        </div>
-      );
-    }
-    else{
-      return(
-        <div className="App">
-            <h1>frigg</h1>
-        </div>
-      );
-    }
+
+
+    return Colls;
   }
 }
 
--- server/routes/ServerIndex.js
+++ server/routes/ServerIndex.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json({test : 'true'});
+  res.json({GridList: [
+    {Name: "bertus", Value: 32, Tier: 2, Grid:[0,2]},
+    {Name: "bert", Value: 33, Tier: 2, Grid:[0,0]},
+    {Name: "Jack", Value: 12, Tier: 2, Grid:[1,0]},
+    {Name: "Jauk", Value: 82, Tier: 2, Grid:[1,1]}
+  ]});
 });
 
 module.exports = router;
